0508e10cdc1d70030ff401cc02f0e1f4041e1683
lemondreamtobe <897418436@qq.com>
2018-06-21 11:39:36 +0800

    整个架构改变 并且增加首页

---
+++ common/js/url.js
@@ -0,0 +1,65 @@
+(function () {
+    window.index_url = [{
+            id: 1,
+            pId: 0,
+            name: "2018年6月",
+            open: true
+        },
+        {
+            id: 11,
+            pId: 1,
+            name: "19日",
+            open: true
+        },
+        {
+            id: 111,
+            pId: 11,
+            name: "判断字符串中出现最多的字符并统计数量",
+            url: './2018.6/19/zwl.html',
+        },
+        {
+            id: 112,
+            pId: 11,
+            name: "对于编程的理解",
+            url: './2018.6/19/zlf.html',
+        },
+        {
+            id: 12,
+            pId: 1,
+            name: "20日",
+            open: true
+        },
+        {
+            id: 121,
+            pId: 12,
+            name: "Object.create和instanceof语句的原理",
+            url: './2018.6/20/zlf.html',
+        },
+        {
+            id: 122,
+            pId: 12,
+            name: "简述一下src与href的区别",
+            url: './2018.6/20/zwl.html',
+        },
+        {
+            id: 13,
+            pId: 1,
+            name: "21日",
+            isParent: true,
+            open: true
+        },
+        {
+            id: 131,
+            pId: 13,
+            name: "tcp为什么是三次握手，四次挥手？",
+            url: './2018.6/21/xmh.html',
+        },
+        {
+            id: 132,
+            pId: 13,
+            name: "Js和DOM深拷贝和浅拷贝的区分",
+            url: './2018.6/21/zlf.html',
+            // open: true
+        }, 
+    ]
+})()
